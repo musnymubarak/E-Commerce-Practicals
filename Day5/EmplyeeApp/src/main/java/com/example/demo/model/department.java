0ec76c640fd8685f939515c7db3d0087d710273f
musnymubarak <musnymohammed@gmail.com>
2025-05-24 21:39:10 +0530

    department model created

---
+++ Day5/EmplyeeApp/src/main/java/com/example/demo/model/department.java
@@ -0,0 +1,19 @@
+package com.example.demo.model;
+
+import java.util.List;
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class department {
+	@Id
+	@Column(name="depy_id")
+	private int id;
+	private String name;
+	private Date established;
+	
+}
