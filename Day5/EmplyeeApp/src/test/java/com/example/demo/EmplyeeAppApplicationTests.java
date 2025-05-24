1d71ca52d171b8ca1954d54c1c892f04bb7e26f1
musnymubarak <musnymohammed@gmail.com>
2025-05-24 21:34:44 +0530

    Initialize spring app

---
+++ Day5/EmplyeeApp/src/test/java/com/example/demo/EmplyeeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmplyeeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
